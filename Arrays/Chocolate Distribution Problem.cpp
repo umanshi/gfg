2b9c30889e1fe3d333a3efb62c7f4e6a89649880
umanshi <b.umanshi@gmail.com>
2019-04-02 20:58:44 +0530

    Create Chocolate Distribution Problem.cpp

---
+++ Arrays/Chocolate Distribution Problem.cpp
@@ -0,0 +1,46 @@
+/*Sort the array,
+USe Sliding window approach
+for every window find the diff between the minimum and maximum elt
+return the min diff all windows
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for (auto i = (a); i <= (b); i++)
+#define NFOR(i,a,b) for(auto i = (a); i >= (b); i++)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(c) (c).begin(),(c).end()
+#define allr(c) (c).rbegin(),(c).rend()
+#define max3(a,b,c) (max(max(a,b),c))
+#define min3(a,b,c) (min(min(a,b),c))
+#define pii pair<int,int>
+typedef long double ld;
+typedef long long int ll;
+ll gcd(ll x,ll y){if(!x||!y) return x+y; return x%y==0?y:gcd(y,x%y);}
+ll lcm(ll x,ll y){return x*(y/gcd(x,y));}
+void printA(ll A[], ll n){for(ll i=0;i<n; i++) cout<<A[i]<<" "; cout<<endl;}
+
+int main()
+{
+	int T; cin>>T;
+	while(T--)
+	{
+		int n; cin>>n;
+		int A[n]; 
+		for(int i=0;i<n; i++)   cin>>A[i];
+		int m; cin>>m;
+		sort(A, A+n);
+		int mindiff = INT_MAX;
+		for(int i=0; i<=n-m; i++)
+		{
+		    int h = i+m-1;
+		    int currdiff = A[h]-A[i];
+		    if(currdiff<mindiff)
+		        mindiff = currdiff;
+		}
+		cout<<mindiff<<endl;
+	}
+	return 0;
+}
